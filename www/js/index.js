38926927e41ada594649c1e88e501054e2e98dab
sashekumar <sashekumar@gmail.com>
2015-05-30 21:30:27 +0800

    Commit 2
    Added GSM

--- www/js/index.js
+++ www/js/index.js
@@ -18,28 +18,28 @@
  */
 var app = {
     // Application Constructor
-    initialize: function() {
+    initialize: function () {
         this.bindEvents();
     },
     // Bind Event Listeners
     //
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
+    bindEvents: function () {
         document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
+    onDeviceReady: function () {
         app.receivedEvent('deviceready');
         var pushNotification = window.plugins.pushNotification;
-        pushNotification.register(app.successHandler, app.errorHandler,{"senderID":"824841663931","ecb":"app.onNotificationGCM"});
+        pushNotification.register(app.successHandler, app.errorHandler, { "senderID": "433350036221", "ecb": "app.onNotificationGCM" });
 
     },
     // Update DOM on a Received Event
-    receivedEvent: function(id) {
+    receivedEvent: function (id) {
         var parentElement = document.getElementById(id);
         var listeningElement = parentElement.querySelector('.listening');
         var receivedElement = parentElement.querySelector('.received');
@@ -50,30 +50,28 @@ var app = {
         console.log('Received Event: ' + id);
     },
     // result contains any message sent from the plugin call
-    successHandler: function(result) {
-        alert('Callback Success! Result = '+result)
+    successHandler: function (result) {
+        alert('Callback Success! Result = ' + result)
     },
-    errorHandler:function(error) {
+    errorHandler: function (error) {
         alert(error);
     },
-    onNotificationGCM: function(e) {
-        switch( e.event )
-        {
+    onNotificationGCM: function (e) {
+        switch (e.event) {
             case 'registered':
-                if ( e.regid.length > 0 )
-                {
+                if (e.regid.length > 0) {
                     console.log("Regid " + e.regid);
-                    alert('registration id = '+e.regid);
+                    alert('registration id = ' + e.regid);
                 }
                 break;
 
             case 'message':
                 // this is the actual push notification. its format depends on the data model from the push server
-                alert('message = '+e.message+' msgcnt = '+e.msgcnt);
+                alert('message = ' + e.message + ' msgcnt = ' + e.msgcnt);
                 break;
 
             case 'error':
-                alert('GCM error = '+e.msg);
+                alert('GCM error = ' + e.msg);
                 break;
 
             default:
